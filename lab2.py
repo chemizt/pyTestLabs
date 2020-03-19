e44b26ab49f62a18f7a4d7d0c06f1df51ada3b39
Michael Slinkin <michaelnslinkin@gmail.com>
2020-03-19 17:33:50 +0300

    added lab2

--- lab2.py
+++ lab2.py
@@ -1,6 +1,32 @@
+import time
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 
-driver.close()
+sites = []
+sitesDict = {}
+
+with open("resources/lab2.txt") as f:
+    for string in f.readlines():
+        sites.append(string.replace("\n", ""))
+
+sites.sort(key=len)
+i = 1
+
+for x in sites:
+    driver.execute_script("window.open('" + x + "');")
+    sitesDict[x] = driver.window_handles[i]
+    i = i + 1
+    time.sleep(0.5)
+
+sites.sort()
+
+for x in sites:
+    print(sitesDict[x])
+
+for x in sites:
+    driver.switch_to.window(window_name=sitesDict[x])
+    driver.close()
+
+driver.quit()
